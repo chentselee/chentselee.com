6cd2faee45beee9fe87a5a986fc81662728b4346
承澤李 <chentselee@gmail.com>
2020-06-15 03:42:11 +0800

    Add head

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -6,6 +7,13 @@ const Main = ({ children }) => {
   return (
     <>
       <div>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>Chentse Lee</title>
+        </Head>
         <Navbar />
         <main>{children}</main>
         <Footer />
